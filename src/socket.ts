39832dd66905612b381370c4be94303fb3a0abcc
Cristian Palacio <cristianpalacios193@gmail.com>
2025-12-11 13:43:24 -0500

    socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
 export const socket = io("https://backendsimplegest.onrender.com",{
-    transports: ["websocket"],
     withCredentials: true,
+    transports: ["polling", "websocket"],
 })
